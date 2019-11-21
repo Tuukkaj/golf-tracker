f0a362c24d023448bee72ebc120612242fe68a8f
Tuukkaj <juusela.tuukka@gmail.com>
2019-11-21 18:39:18 +0200

    Initial project creation

---
+++ golf-tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  golf-tracker
+//
+//  Created by Tuukka Juusela on 21.11.2019.
+//  Copyright © 2019 Tuukka Juusela. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
