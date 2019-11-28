2ab06253dd1cfa0bf72a22d7927ab480118a6308
Tuukkaj <juusela.tuukka@gmail.com>
2019-11-28 17:26:04 +0200

    Add ability to add points to played hole

--- golf-tracker/Views/CoursePlay.swift
+++ golf-tracker/Views/CoursePlay.swift
@@ -10,11 +10,21 @@ import SwiftUI
 
 struct CoursePlay: View {
     @State var totalPar = 0
+    @State var selected = 0
+    @State var binders = [String]()
+    @State var input : String = ""
+    @ObservedObject var play = playState
     var currentScore = 0
     
-    func updateScores() {
+    func onSetScore() {
+        if let num = Int(self.input) {
+            play.played[self.selected] = num
+            self.selected = self.selected < playState.played.count - 1 ? self.selected + 1 : playState.played.count - 1
+        }
         
+        self.input = ""
     }
+    
     var body: some View {
         var currentScore = 0
         
@@ -39,6 +49,51 @@ struct CoursePlay: View {
             Divider()
             Text("Course par: \(totalPar)")
             Text("Score: \(totalPar)")
+            
+            HStack {
+                Spacer()
+                Button(action: {self.selected = self.selected > 0 ? self.selected - 1 : 0}) {
+                    Image(systemName: "backward.fill")
+                    Text("Prev")
+                        .font(.title)
+                }
+                Text("\(self.selected + 1)")
+                    .font(.title)
+                
+                Button(action: {self.selected = self.selected < playState.played.count - 1 ? self.selected + 1 : playState.played.count - 1}) {
+                    Text("Next")
+                        .font(.title)
+                    Image(systemName: "forward.fill")
+                }
+                Spacer()
+            }
+            
+            HStack {
+                TextField("Enter score",text: $input)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .keyboardType(.numberPad)
+                Button(action: onSetScore) {
+                    Text("Set score")
+                }                
+            }
+            
+            Divider()
+            
+            List {
+                ForEach(playState.holes.indices, id: \.self) { i in
+                    Button(action: {self.selected = i}) {
+                        HStack {
+                            if i == self.selected {
+                                Image(systemName: "arrowtriangle.right.fill")
+                            }
+                            
+                            Text("\(i + 1) - ").fontWeight(i == self.selected ? .bold : .none)
+                            Text("Par: \(playState.holes[i]) - ").fontWeight(i == self.selected ? .bold : .none)
+                            Text("Score: \(self.play.played[i] != nil ? String(self.play.played[i]!) : "Not played")").fontWeight(i == self.selected ? .bold : .none)
+                            }
+                    }.padding()
+                }
+            }
             
         }.frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
         .padding()
